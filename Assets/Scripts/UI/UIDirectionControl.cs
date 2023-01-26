a047a54c7b6da8c961f04cac6612ea2a905852b7
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-01-26 11:25:20 +0200

    finished working on the health wheel unity editor side of things

--- Assets/Scripts/UI/UIDirectionControl.cs
+++ Assets/Scripts/UI/UIDirectionControl.cs
@@ -10,12 +10,16 @@ public class UIDirectionControl : MonoBehaviour
 
     private void Start()
     {
+        // Get the rotation value of the parent canvas
         m_RelativeRotation = transform.parent.localRotation;
     }
 
 
     private void Update()
     {
+        // reapply the start rotation value every frame
+        // so the health wheel doesn't rotate indebented from the tank
+        // Keeping it fixed in place
         if (m_UseRelativeRotation)
             transform.rotation = m_RelativeRotation;
     }
